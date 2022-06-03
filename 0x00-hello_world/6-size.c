8ec5ff23983d06026b0828ebb91c490ea95c6ebe
Stephen <ayubastephen212@gamil.com>
2022-06-02 21:55:06 -0700

    Update repo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,6 @@ int main(void)
 	printf("Size of an int: %zu byte(s)", sizeof(int));
 	printf("Size of a long int: %zu byte(s)", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)", sizeof(float))
+	printf("Size of a float: %zu byte(s)", sizeof(float));
 	return (0);
 }
