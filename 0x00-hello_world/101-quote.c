70a54f283fad7a68c71a60f2fe69573f16a36f85
Stephen <ayubastephen212@gamil.com>
2022-06-05 20:35:32 -0700

    Add new command script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Prints - Programming is like a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	
+	return (0);
+}
