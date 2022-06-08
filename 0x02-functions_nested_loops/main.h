09946b06705badbc78df676b7b6b28d1370f83dd
Stephen <ayubastephen212@gamil.com>
2022-06-07 22:03:22 -0700

    Edit header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,11 +6,12 @@
  * Return 0 (Success)
  */
 
-int _putchar(char aa)                                                 
+int _putchar(char a)                                                 
 {                                                                    
 	putchar(a);
 }
 
+int _putchar(char a);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
