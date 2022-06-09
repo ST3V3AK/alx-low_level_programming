f814184fb353f5f4b86712e8517882cd9c5a182a
Stephen <ayubastephen212@gamil.com>
2022-06-08 21:43:36 -0700

    Edit 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -33,7 +33,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			if ((val < 10) && (i < 9))
+			else if ((val <= 10) && (i < 9))
 			{
 				_putchar(',');
 				_putchar(' ');
