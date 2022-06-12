cb855546cd01f44ab2ccba9770aeecb18b8959d5
Stephen <ayubastephen212@gamil.com>
2022-06-12 04:53:24 -0700

    Edit program files

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 {
 	int n = 0;
 
-	while (n < 0)
+	while (n < 10)
 	{
 		if (n != 2 && n != 4)
 			_putchar(n % 10 + '0');
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,9 @@
 
 void more_numbers(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
 		int n = 0;
 
