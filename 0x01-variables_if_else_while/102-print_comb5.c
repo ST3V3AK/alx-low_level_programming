e81f6d7291c62df7ad2eb84db5d8dbd7ecf7492d
Stephen <ayubastephen212@gamil.com>
2022-06-27 03:48:19 -0700

    Edit 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -9,42 +7,27 @@
  */
 int main(void)
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int d = 0;
+	int a;
+	int b;
 
-	while (a < 10)
+	for (a = 0; a < 100; a++)
 	{
-		while (b < 10)
+		for (b = 0; b < 100; b++)
 		{
-			while (c < 10)
+			if (a < b)
 			{
-				while (d < 10)
+				putchar(a / 10 + '0');
+				putchar(a % 10 + '0');
+				putchar(' ');
+				putchar(b / 10 + '0');
+				putchar(b % 10 + '0');
+				if (a + b < 197)
 				{
-					if (a + b <= c + d && a + b != c + d)
-					{
-						putchar(a % 10 + '0');
-						putchar(b % 10 + '0');
-						putchar(' ');
-						putchar(c % 10 + '0');
-						putchar(d % 10 + '0');
-						if (a + b != 17)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-					d++;
+					putchar(',');
+					putchar(' ');
 				}
-				d = 0;
-				c++;
 			}
-			c = 0;
-			b++;
 		}
-		b = 0;
-		a++;
 	}
 	putchar('\n');
 	return (0);
