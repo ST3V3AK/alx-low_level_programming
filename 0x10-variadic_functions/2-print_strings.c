e12b6b36371af8fdf691e9a63e19644d928e4f35
Stephen <ayubastephen212@gamil.com>
2022-08-07 12:56:12 -0700

    Edit and complete all program files

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print n number of strings connected by a seoarator
+ * @separator: separator
+ * @n: number of strings
+ *
+ * Return: void.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list str;
+	char *s;
+	unsigned int i;
+
+	va_start(str, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(str, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	putchar('\n');
+	va_end(str);
+	return;
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,56 @@
+#include <stdarg.h>
+
+#include <stdio.h>
+#include <string.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints the refeence to a list of arguments
+ * @format: list of arguments
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list arg;
+	char *str;
+	int n;
+	int i;
+
+	va_start(arg, format);
+
+	i = 0;
+	while (format[i])
+	{
+		n = strlen(format) - 1;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(arg, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(arg, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(arg, double));
+				break;
+			case 's':
+				str = va_arg(arg, char *);
+				if (str == NULL)
+					printf("(nil)");
+				else
+					printf("%s", str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (format[i] != format[n])
+			printf(", ");
+		i++;
+	}
+	va_end(arg);
+	putchar('\n');
+	return;
+}
